3a3e4bc377da419cca4a573e02e5c576029203fe
Emilya-Grigiryan <161468131+Emilya-Grigiryan@users.noreply.github.com>
2024-07-26 14:36:22 +0400

    Create list_sort.js

---
+++ list_sort.js
@@ -0,0 +1,12 @@
+let marr = [21, 58, 69, 12, 35, 5];
+let tmp = 0;
+for (let i = 0; i < marr.length; i++) {
+    for (let j = 0; j < marr.length - i - 1; j++) {
+        if (marr[j] > marr[j+1]) {
+            tmp = marr[j];
+            marr[j] = marr[j+1];
+            marr[j+1] = tmp;
+        }
+    }
+}
+console.log(marr);
