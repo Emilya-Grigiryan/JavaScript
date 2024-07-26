d882830e2bad15e64026a72613613f4f6ad640fb
Emilya-Grigiryan <161468131+Emilya-Grigiryan@users.noreply.github.com>
2024-07-26 14:35:08 +0400

    Create valid_parentheses.js

---
+++ valid_parentheses.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const bracketPairs = {')': '(', '}': '{', ']': '['};
+    const stack = [];
+    for (const char of s) {
+        if (bracketPairs[char]) {
+            const element = stack.length ? stack.pop() : '#';
+            if (bracketPairs[char] !== element) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+};
